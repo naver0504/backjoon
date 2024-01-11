a781995a79b11ddde17a48875183a509a51da958
naver0504 <qortndwls1234@gmail.com>
2024-01-12 02:52:47 +0900

    10주차 문제

---
+++ src/week10/카드합체놀이_15903.java
@@ -0,0 +1,38 @@
+package week10;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class 카드합체놀이_15903 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        PriorityQueue<Long> pq = new PriorityQueue<>(n);
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            pq.add(Long.parseLong(st.nextToken()));
+        }
+
+        for (int i = 0; i < m; i++) {
+            long a = pq.poll();
+            long b = pq.poll();
+            for (int j = 0; j < 2; j++) {
+                pq.add(a + b);
+            }
+        }
+
+        long result = 0;
+        for (Long value : pq) {
+            result += value;
+        }
+
+        System.out.println(result);
+
+    }
+}
